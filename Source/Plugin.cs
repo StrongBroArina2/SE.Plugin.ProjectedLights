f39bde667f847ece100e870c51ee021a1561f038
StrongBroArina2 <arina.lokhovskaya@mail.ru>
2025-10-12 03:51:22 +0300

    Fix Plugin Dependencies

--- Source/Plugin.cs
+++ Source/Plugin.cs
@@ -1,5 +1,4 @@
-﻿using avaness.PluginLoader;
-using HarmonyLib;
+﻿using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,74 +8,26 @@ using VRage.Utils;
 
 namespace mleise.ProjectedLightsPlugin
 {
-	sealed class Main : IPlugin
-	{
-		/// <summary>Resolves the Pulsar plugin loader assembly when Init() is called. (Required when compiled from development folder.)</summary>
-		static Main()
-		{
-			AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler((object sender, ResolveEventArgs args) =>
-			{
-				if (args.RequestingAssembly == Assembly.GetExecutingAssembly() && args.Name.StartsWith("PluginLoader, "))
-				{
-					return Assembly.LoadFrom(Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "Plugins", "loader.dll"));
-				}
-				return null;
-			});
-		}
+    sealed class Main : IPlugin
+    {
+      public void Init(object gameInstance)
+        {
+            // Ensure that we aren't loading twice. Not anymore pal
 
-		/// <summary>Called when the plugins are instantiated.</summary>
-		public void Init(object gameInstance)
-		{
-			// Ensure that we aren't loading twice.
-			var allPlugins = new List<KeyValuePair<IPlugin, bool>>();
+            // Patch Space Engineers.
+            Harmony harmony = new Harmony(typeof(Main).Namespace);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
 
-			foreach (var plugin in MyPlugins.Plugins)
-			{
-				if (plugin is avaness.PluginLoader.Main)
-				{
-					var pluginField = AccessTools.Field(typeof(PluginInstance), "plugin");
-					foreach (var pluginInstance in (List<PluginInstance>)AccessTools.Field(typeof(avaness.PluginLoader.Main), "plugins").GetValue(plugin))
-					{
-						allPlugins.Add(new KeyValuePair<IPlugin, bool>((IPlugin)pluginField.GetValue(pluginInstance), false));
-					}
-				}
-				else
-				{
-					allPlugins.Add(new KeyValuePair<IPlugin, bool>(plugin, true));
-				}
-			}
+            // Space Engineers compiles a couple of shaders before plugins have a chance to intercept them.
+            // By triggering a recompile, we can go back and patch them. They will eventually all be cached in
+            // "%APPDATA%\SpaceEngineers\ShaderCache2".
+            AccessTools.Method("VRageRender.MyPixelShaders:Recompile").Invoke(null, null);
+        }
 
-			foreach (var plugin in allPlugins)
-			{
-				if (ReferenceEquals(plugin.Key, this))
-				{
-					break;
-				}
-				else if (plugin.Key.GetType().Namespace == typeof(Main).Namespace)
-				{
-					var assembly = plugin.Key.GetType().Assembly;
-					var location = assembly.Location;
-					var msg = "A version of \"" + assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title + "\" has already been instantiated via the ";
-					msg += (plugin.Value ? "command-line" : "Plugin Loader") + ":" + (location == "" ? " Compiled from development folder." : "\n" + location);
-					MyMessageBox.Show("Plugin patching aborted", msg);
-					throw new Exception(msg);
-				}
-			}
+        /// <summary>Called when the game is closed.</summary>
+        public void Dispose() { }
 
-			// Patch Space Engineers.
-			Harmony harmony = new Harmony(typeof(Main).Namespace);
-			harmony.PatchAll(Assembly.GetExecutingAssembly());
-
-			// Space Engineers compiles a couple of shaders before plugins have a chance to intercept them.
-			// By triggering a recompile, we can go back and patch them. They will eventually all be cached in
-			// "%APPDATA%\SpaceEngineers\ShaderCache2".
-			AccessTools.Method("VRageRender.MyPixelShaders:Recompile").Invoke(null, null);
-		}
-
-		/// <summary>Called when the game is closed.</summary>
-		public void Dispose() {}
-
-		/// <summary>Called at the end of each game update at 60 Hz, with only audio trailing it.</summary>
-		public void Update() {}
-	}
+        /// <summary>Called at the end of each game update at 60 Hz, with only audio trailing it.</summary>
+        public void Update() { }
+    }
 }
